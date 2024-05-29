1a8813bb6472439a2625ba9867dbe1f0ca324532
Rachele-Turtora <racheleturtora21@gmail.com>
2024-05-29 19:51:39 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+    name: 'MyApp',
+    data() {
+        return {
+      };
+  },
+
+  components: {
+      AppTitle,
+  }
+}
+</script>
+
+<template>
+  <AppTitle />
+</template>
+
+<style scoped>
+
+</style>
